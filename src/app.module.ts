21505d6c18395da71ee8975dd2d3150e27874d6e
Leonan Martins <leonanm04@gmail.com>
2023-07-13 16:54:17 -0300

    fix: prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { PublicationModule } from './modules/publication/publication.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, PublicationModule],
+  imports: [UserModule, PublicationModule, PrismaModule],
   controllers: [],
   providers: [],
 })
